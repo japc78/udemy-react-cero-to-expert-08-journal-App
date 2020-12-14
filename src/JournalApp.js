f6c93e6ea3a0aff2145a01dfd877def60866167e
japc <japc.grafico@gmail.com>
2020-12-14 13:47:32 +0100

    Redux Setting

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import { AppRouters } from './components/routers/AppRouters'
+import { store } from './store/store'
 
 export const JournalApp = () => {
-    return <AppRouters/>
+    return (
+        <Provider store= { store }>
+            <AppRouters/>
+        </Provider>
+    )
 }
