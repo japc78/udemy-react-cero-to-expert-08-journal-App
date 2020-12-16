3701ec752540f21a914fd3f449607841cea24a44
japc <japc.grafico@gmail.com>
2020-12-16 13:03:53 +0100

    Logout on firebase

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -66,3 +66,17 @@ export const login = (uid, displayName) => {
         }
     }
 }
+
+
+export const startLogout = () => {
+    return async(dispatch) => {
+        await firebase.auth().signOut();
+
+        dispatch( logout() );
+    }
+}
+
+
+export const logout = () => ({
+    type: types.logout
+})
