64ec76a8cdbcf3f2e09a9d1765cd7a91f3da4e9c
japc <japc.grafico@gmail.com>
2020-12-22 16:49:27 +0100

    Activation of the note for editing

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { useForm } from '../../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar'
 
 export const NoteScreen = () => {
+
+    const { active: note } = useSelector(state => state.notes);
+    const [formValues, handleInputChange] = useForm( note );
+
+    const { title, body, url } = formValues;
+
+    // date, title, body, url
+
     return (
         <div className="notes__main-content">
-            <NotesAppBar/>
+            <NotesAppBar />
 
             <div className="notes__content">
                 <input
@@ -12,19 +22,27 @@ export const NoteScreen = () => {
                     placeholder="Some awesome title"
                     className="notes__title-input"
                     autoComplete="off"
+                    onChange = { handleInputChange }
+                    value={ title }
                 />
 
                 <textarea
                     placeholder="What happened today"
                     className="notes__textarea"
+                    onChange = { handleInputChange }
+                    value={ body }
                 ></textarea>
 
-                <div className="notes__image">
-                    <img
-                        src="https://i.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68"
-                        alt="Picsum"
-                    />
-                </div>
+                {
+                    url &&
+                    <div className="notes__image">
+                        <img
+                            src={ url }
+                            alt={ title }
+                        />
+                    </div>
+                }
+
             </div>
         </div>
     )
